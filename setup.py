99984532c7aa445ad150de84227457c53d06e299
Danijar Hafner <mail@danijar.com>
2022-08-24 19:12:54 +0100

    Update dependencies.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pathlib
 
 setuptools.setup(
     name='npgame',
-    version='0.1.0',
+    version='0.1.1',
     author='Danijar Hafner',
     author_email='mail@danijar.com',
     description='Write simple games in Numpy!',
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=['pygame', 'numpy'],
+    install_requires=['pygame', 'numpy', 'pillow'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
