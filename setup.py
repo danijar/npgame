f9135938fbe716ae56e231795641dc5f5472d834
Danijar Hafner <mail@danijar.com>
2022-08-25 14:48:38 +0100

    Fix installation on Windows.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import pathlib
 
 setuptools.setup(
     name='npgame',
-    version='0.2.0',
+    version='0.2.1',
     author='Danijar Hafner',
     author_email='mail@danijar.com',
     description='Write simple games in Numpy!',
     url='http://github.com/danijar/npgame',
-    long_description=pathlib.Path('README.md').read_text(),
+    long_description=pathlib.Path('README.md').read_bytes().decode('utf-8'),
     long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(),
+    packages=['npgame'],
     include_package_data=True,
     install_requires=['pygame', 'numpy', 'pillow'],
     classifiers=[
